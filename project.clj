027cdcfa296c09d31140b2594867eabedaeeb581
Dean Johnson <d.johnson@marval.co.uk>
2019-02-16 18:46:23 +0000

    Added base functionality, still need to add some more and clean up repeated code

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://someawesomesite.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "3.9.1"]]
   :repl-options {:init-ns clohue.core})
--- src/clohue/core.clj
+++ src/clohue/core.clj
@@ -10,28 +10,102 @@
 
 (def bridge (bridge-memo))
 
-(defn create-user [{ip :internalipaddress} device-type]
+(defn create-user [bridge device-type]
   "Create a new user to use to authenticate against the Hue Bridge"
-  (first (json/read-json (:body (client/post (str "http://" ip "/api")
-                                             {:body (json/write-str {:devicetype device-type})
-                                              :content-type :json
-                                              :accept :json})))))
+  (let [{:keys [id internalipaddress]} bridge]
+    (first (json/read-json (:body (client/post (str "http://" internalipaddress "/api")
+                                               {:body (json/write-str {:devicetype device-type})
+                                                :content-type :json
+                                                :accept :json}))))))
 
-(defn toggle-light-state [{ip :internalipaddress} username light-id on?]
-  (first (json/read-json (:body (client/put (str "http://" ip "/api/" username "/lights/" light-id "/state")
-                                            {:body (json/write-str {:on on?})
-                                             :content-type :json
-                                             :accept :json})))))
+
+(defn get-users [bridge username]
+  (let [{:keys [id internalipaddress]} bridge]
+    (:whitelist (json/read-json (:body (client/get (str "http://" internalipaddress "/api/" username "/config")))))))
+
+
+(defn delete-user [bridge username username-to-delete]
+  (let [{:keys [id internalipaddress]} bridge]
+    (json/read-json (:body (client/delete (str "http://" internalipaddress "/api/" username "/config/whitelist/" username-to-delete))))))
+
+
+(defn toggle-light-state [bridge username light-id on?]
+  (let [{:keys [id internalipaddress]} bridge]
+    (first (json/read-json (:body (client/put (str "http://" internalipaddress "/api/" username "/lights/" light-id "/state")
+                                              {:body (json/write-str {:on on?})
+                                               :content-type :json
+                                               :accept :json}))))))
+
 
 (defn get-light-status [{ip :internalipaddress} username light-id]
   (json/read-json (:body (client/get (str "http://" ip "/api/" username "/lights/" light-id)))))
 
 
+(defn get-lights [bridge username]
+  (let [{:keys [id internalipaddress]} bridge]
+    (json/read-json (:body (client/get (str "http://" internalipaddress "/api/" username "/lights"))))))
+
+
+
+(defn change-light-colour [bridge username light-id hue sat]
+  (let [{:keys [id internalipaddress]} bridge]
+    (json/read-json (:body (client/put (str "http://" internalipaddress "/api/" username "/lights/" light-id "/state")
+                                       {:body (json/write-str {:hue hue
+                                                               :sat sat})
+                                        :content-type :json
+                                        :accept :json})))))
+
+(defn change-light-temperature [bridge username light-id temp]
+  (let [{:keys [id internalipaddress]} bridge]
+    (json/read-json (:body (client/put (str "http://" internalipaddress "/api/" username "/lights/" light-id "/state")
+                                       {:body (json/write-str {:ct temp})
+                                        :content-type :json
+                                        :accept :json})))))
+
+
+(defn send-alert [bridge username light-id]
+  (let [{:keys [id internalipaddress]} bridge
+        light-status (get-light-status bridge username light-id)]
+    (json/read-json (:body (client/put (str "http://" internalipaddress "/api/" username "/lights/" light-id "/state")
+                                       {:body (json/write-str {:on true
+                                                               :hue 20
+                                                               :sat 254
+                                                               :alert :lselect})
+                                        :content-type :json
+                                        :accept :json})))
+    (Thread/sleep 15000)
+    (json/read-json (:body (client/put (str "http://" internalipaddress "/api/" username "/lights/" light-id "/state")
+                                       {:body (json/write-str {:on (:on (:state light-status))
+                                                               :hue (:hue (:state light-status))
+                                                               :sat (:sat (:state light-status))})
+                                        :content-type :json
+                                        :accept :json})))))
+
+
+(defn change-light-brightness [bridge username light-id bri]
+  (let [{:keys [id internalipaddress]} bridge]
+    (json/read-json (:body (client/put (str "http://" internalipaddress "/api/" username "/lights/" light-id "/state")
+                                       {:body (json/write-str {:bri bri})
+                                        :content-type :json
+                                        :accept :json})))))
+
+
+
+(change-light-temperature bridge user 1 350)
+
+(change-light-brightness bridge user 1 254)
+
+(send-alert bridge user 1)
+
+(change-light-colour bridge user 1 5600 254)
+
 (toggle-light-state bridge user 1 true)
 
 (get-light-status bridge user 1)
 
+(get-users bridge user)
 
+(get-lights bridge user)
 
 
 
@@ -42,11 +116,11 @@
 
 ;; [{:success {:username 7ATuEm2fRARZFaUPrQagul0OEMWCRjTDLfpR2b9-}}]
 
-(defn store-username [username])
+;; (defn store-username [username])
 
-(defn- read-username [])
+;; (defn- read-username [])
 
-(def username (memoize read-username))
+;; (def username (memoize read-username))
 
 ;; (def response (create-user bridge "clojure-app"))
 
